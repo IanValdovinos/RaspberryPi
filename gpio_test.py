75ef2d600ffa72bbb81870b60007fff8a0fa6671
Ian Valdovinos <ian.valdovinos752001@gmail.com>
2021-05-10 17:21:45 -0400

    Create reference

--- gpio_test.py
+++ gpio_test.py
@@ -1,3 +1,27 @@
 import RPi.GPIO as GPIO
 
 print("Import successful")
+
+"""
+Select pin numbering mode:
+GPIO.setmode(GPIO.BOARD)
+or
+GPIO.setmode(GPIO.BCM)
+"""
+GPIO.setmode(GPIO.BOARD)
+
+"""
+Setup a pin for input or output:
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(13, GPIO.IN)
+"""
+
+"""
+Assign a value to an outpin pin:
+GPIO.output(13, True)
+"""
+
+"""
+Reading input pins:
+value = GPIO.input(13)
+"""
