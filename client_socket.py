64f000ebdf77ca2cb6269579af4bd027a3f72f90
Ian Valdovinos <ian.valdovinos752001@gmail.com>
2021-05-18 22:06:15 -0400

    Add notes on network client socket

--- client_socket.py
+++ client_socket.py
@@ -10,4 +10,23 @@ This program creates a generic network client
 
 import socket
 
+# 1. Create a socket
 mysock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# AF_INET declares Address Family Internet
+# SOCK_STREAM indicates TCP
+
+# 2. Connect socket to server
+host = socket.gethostbyname("www.google.com") # Gets IP address by a domain name 
+
+mysock.connect((host, 80)) # Connect client to host. 80 refers to port 8 for web traffic
+
+# 3. Send some data
+message = "GET / HTTP/1.1\r\n\r\n" # HTTP GET request
+
+mysock.sendall(message)
+
+# 4. Receive some data
+data = mysock.recv(1000) # Argument is the maximum number of bytes expected
+
+# 5. Closing a socket
+mysock.close()
